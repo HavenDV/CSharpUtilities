5f16ecf1a3e9614723ef2e9ba5c2a786bee311f2
Konstantin S <havendv@gmail.com>
2020-02-04 02:52:54 +1000

    Updated TaskWorker

--- NetStandard21/Utilities/TaskWorker.cs
+++ NetStandard21/Utilities/TaskWorker.cs
@@ -9,7 +9,7 @@ namespace NetStandard21.Utilities
     /// <summary>
     /// A class designed to run code using <see cref="Task"/> with <see cref="TaskCreationOptions.LongRunning"/> <br/>
     /// and supporting automatic cancellation after <see cref="DisposeAsync"/>
-    /// <![CDATA[Version: 1.0.0.2]]>
+    /// <![CDATA[Version: 1.0.0.3]]>
     /// </summary>
     internal class TaskWorker : IDisposable, IAsyncDisposable
     {
@@ -71,7 +71,7 @@ namespace NetStandard21.Utilities
             await Task.ConfigureAwait(false);
 
             // Some system code can still use CancellationToken, so we wait
-            await Task.Delay(1);
+            await Task.Delay(TimeSpan.FromMilliseconds(1)).ConfigureAwait(false);
 
             CancellationTokenSource.Dispose();
             Task.Dispose();
