1726650f58108858cd822b93662aa546a81fdf09
Konstantin S <havendv@gmail.com>
2020-02-04 11:59:56 +1000

    Added Uri extensions tests

---
+++ NetStandard20.Tests/UriExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetStandard20.Extensions;
+
+namespace NetStandard20.Tests
+{
+    [TestClass]
+    public class UriExtensionsTests
+    {
+        [TestMethod]
+        public void WithQueryTest()
+        {
+            Assert.AreEqual(
+                new Uri("https://www.unibet.ie/kambi-rest-api/gameLauncher2.json?_=1574752540405&useRealMoney=true&brand=unibet&jurisdiction=IE&locale=en_IE&currency=EUR&deviceGroup=desktop&clientId=polopoly_desktop&deviceOs=&marketLocale=en_IE&loadHTML5client=true&enablePoolBetting=false"),
+                new Uri("https://www.unibet.ie/kambi-rest-api/gameLauncher2.json").WithQuery(new Dictionary<string, string>
+                {
+                    { "_", "1574752540405" },
+                    { "useRealMoney", "true" },
+                    { "brand", "unibet" },
+                    { "jurisdiction", "IE" },
+                    { "locale", "en_IE" },
+                    { "currency", "EUR" },
+                    { "deviceGroup", "desktop" },
+                    { "clientId", "polopoly_desktop" },
+                    { "deviceOs", "" },
+                    { "marketLocale", "en_IE" },
+                    { "loadHTML5client", "true" },
+                    { "enablePoolBetting", "false" },
+                }));
+        }
+
+        [TestMethod]
+        public void WithQueryTest2()
+        {
+            Assert.AreEqual(
+                new Uri("https://al-auth.kambicdn.org/player/api/v2/ubuk/coupon.json;jsessionid={jsessionid}?lang=en_GB&market={market}&client_id=2&channel_id=1&ncid={ncid}"),
+                new Uri("https://al-auth.kambicdn.org/player/api/v2/ubuk/coupon.json;jsessionid={jsessionid}").WithQuery(new Dictionary<string, string>
+                {
+                    { "lang", "en_GB" },
+                    { "market", "{market}" },
+                    { "client_id", "2" },
+                    { "channel_id", "1" },
+                    { "ncid", "{ncid}" },
+                }));
+        }
+
+        [TestMethod]
+        public void WithQueryTest3()
+        {
+            Assert.AreEqual(
+                new Uri("https://www.test.com/betslip/?op=13&ck=bs&betsource=FlashInPLay&streaming=1&fulltext=1&refreshbal=0&isocode=&bc=y&qb=0"),
+                new Uri("https://www.test.com/betslip/").WithQuery(new Dictionary<string, string>
+                {
+                    { "op", "13" },
+                    { "ck", "bs" },
+                    { "betsource", "FlashInPLay" },
+                    { "streaming", "1" },
+                    { "fulltext", "1" },
+                    { "refreshbal", "0" },
+                    { "isocode", "" },
+                    { "bc", "y" },
+                    { "qb", "0" },
+                }));
+        }
+    }
+}
---
+++ NetStandard20/Extensions/UriExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+#nullable enable
+
+namespace NetStandard20.Extensions
+{
+    /// <summary>
+    /// Extensions that work with <see cref="Uri"/>
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Creates uri with query string using values dictionary <br/>
+        /// <![CDATA[Version: 1.0.0.0]]> <br/>
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="dictionary"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
+        public static Uri WithQuery(this Uri uri, Dictionary<string, string> dictionary)
+        {
+            uri = uri ?? throw new ArgumentNullException(nameof(uri));
+            dictionary = dictionary ?? throw new ArgumentNullException(nameof(dictionary));
+
+            var builder = new UriBuilder(uri);
+            var parameters = HttpUtility.ParseQueryString(uri.Query);
+
+            foreach (var pair in dictionary)
+            {
+                parameters[pair.Key] = pair.Value;
+            }
+
+            builder.Query = parameters.ToString();
+
+            return builder.Uri;
+        }
+    }
+}
