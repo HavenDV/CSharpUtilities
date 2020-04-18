7116998196ca91bd675bca4984e5346196be72bd
Konstantin S <havendv@gmail.com>
2020-04-18 18:29:33 +1000

    Added ProcessExtensions.WaitForExitAsync

---
+++ NetStandard20.Tests/ProcessExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetStandard20.Extensions;
+
+namespace NetStandard20.Tests
+{
+    [TestClass]
+    public class ProcessExtensionsTests
+    {
+        [TestMethod]
+        public async Task WaitTest()
+        {
+            using var source = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+            using var process = Process.Start(new ProcessStartInfo("timeout", "2")
+            {
+                UseShellExecute = true,
+            });
+            if (process == null)
+            {
+                throw new InvalidOperationException("Process is null");
+            }
+
+            await process.WaitForExitAsync(source.Token);
+        }
+    }
+}
---
+++ NetStandard20/Extensions/ProcessExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetStandard20.Extensions
+{
+    public static class ProcessExtensions
+    {
+        /// <summary>
+        /// Waits to complete the process.
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="cancellationToken"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="OperationCanceledException"></exception>
+        /// <returns></returns>
+        public static async Task<int> WaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
+        {
+            process = process ?? throw new ArgumentNullException(nameof(process));
+            process.EnableRaisingEvents = true;
+
+            var completionSource = new TaskCompletionSource<int>();
+
+            process.Exited += (sender, args) =>
+            {
+                completionSource.TrySetResult(process.ExitCode);
+            };
+            if (process.HasExited)
+            {
+                return process.ExitCode;
+            }
+
+            using var registration = cancellationToken.Register(
+                () => completionSource.TrySetCanceled(cancellationToken));
+
+            return await completionSource.Task;
+        }
+    }
+}
