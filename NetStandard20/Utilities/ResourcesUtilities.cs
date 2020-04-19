6fc682dc7a554012c7b62b363c631a9630efdad0
Konstantin S <havendv@gmail.com>
2020-04-19 18:07:00 +1000

    Refactored

--- NetStandard20/Utilities/ResourcesUtilities.cs
+++ NetStandard20/Utilities/ResourcesUtilities.cs
@@ -12,7 +12,7 @@ namespace NetStandard20.Utilities
         /// <summary>
         /// Searches for a file among Embedded resources <br/>
         /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
-        /// <![CDATA[Version: 1.0.0.2]]> <br/>
+        /// <![CDATA[Version: 1.0.0.3]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
@@ -26,16 +26,18 @@ namespace NetStandard20.Utilities
 
             try
             {
-                return assembly.GetManifestResourceStream(assembly
-                           .GetManifestResourceNames()
-                           .Single(resourceName => resourceName.EndsWith(name)))
+                return assembly.GetManifestResourceStream(
+                           assembly
+                               .GetManifestResourceNames()
+                               .Single(resourceName => resourceName.EndsWith(name)))
                        ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources");
             }
             catch (InvalidOperationException exception)
             {
                 throw new ArgumentException(
                     "Not a single one was found or more than one resource with the given name was found. " +
-                    "Make sure there are no collisions and the required file has the attribute \"Embedded resource\"", exception);
+                    "Make sure there are no collisions and the required file has the attribute \"Embedded resource\"", 
+                    exception);
             }
         }
 
