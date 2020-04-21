596bf227e7b4ed599f855add29ab20673edc31e5
Konstantin S <havendv@gmail.com>
2020-04-21 15:20:28 +1000

    Fixed NetUtilities tests

--- NetStandard20.Tests/NetUtilitiesTests.cs
+++ NetStandard20.Tests/NetUtilitiesTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetStandard20.Utilities;
@@ -11,10 +12,26 @@ namespace NetStandard20.Tests
         [TestMethod]
         public async Task ReadGetRequestDataAsyncTest()
         {
-            Assert.IsNotNull(await NetUtilities.ReadGetRequestDataAsync(new Uri("https://postman-echo.com/get")));
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            var cancellationToken = cancellationTokenSource.Token;
+
+            Assert.IsNotNull(
+                await NetUtilities.ReadGetRequestDataAsync(
+                    new Uri("https://postman-echo.com/get"), 
+                    cancellationToken: cancellationToken));
+        }
+
+        [TestMethod]
+        public async Task ReadGetRequestDataAsyncTimeoutTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            var cancellationToken = cancellationTokenSource.Token;
 
             await Assert.ThrowsExceptionAsync<TaskCanceledException>(
-                async () => await NetUtilities.ReadGetRequestDataAsync(new Uri("https://postman-echo.com/get"), TimeSpan.FromMilliseconds(1)));
+                async () => await NetUtilities.ReadGetRequestDataAsync(
+                    new Uri("https://postman-echo.com/get"), 
+                    TimeSpan.FromMilliseconds(1), 
+                    cancellationToken));
         }
     }
 }
