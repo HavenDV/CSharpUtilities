0bbe3b5b61c96affb0b3db7f79226782844f590d
Konstantin S <havendv@gmail.com>
2020-05-19 17:40:17 +1000

    Added SynchronizeInvokeExtensions

---
+++ NetCore31.Forms/Extensions/SynchronizeInvokeExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NetCore31.Forms.Extensions
+{
+    /// <summary>
+    /// Extensions that allow you to access the UI stream, if necessary.
+    /// <![CDATA[Version: 1.0.0.0]]> <br/>
+    /// </summary>
+    public static class SynchronizeInvokeExtensions
+    {
+        /// <summary>
+        /// Executes a method in a UI thread.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="action"></param>
+        public static void InvokeIfRequired<T>(this T obj, Action<T> action)
+            where T : ISynchronizeInvoke
+        {
+            if (!obj.InvokeRequired)
+            {
+                action(obj);
+                return;
+            }
+
+            obj.Invoke(action, new object[] { obj });
+        }
+
+        /// <summary>
+        /// Executes a method in a UI thread and returns value.
+        /// </summary>
+        /// <typeparam name="TIn"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static TOut InvokeIfRequired<TIn, TOut>(this TIn obj, Func<TIn, TOut> func)
+            where TIn : ISynchronizeInvoke
+        {
+            return obj.InvokeRequired
+                ? (TOut)obj.Invoke(func, new object[] { obj })
+                : func(obj);
+        }
+    }
+}
