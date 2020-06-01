4fb76dfda95b856eb3107b83a8d387c6b8efb8c0
Konstantin S <havendv@gmail.com>
2020-06-01 10:56:20 +1000

    Added SelectForm

---
+++ NetCore31.Forms/Forms/SelectForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace NetCore31.Forms.Forms
+{
+    partial class SelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 20;
+            this.ListBox.Location = new System.Drawing.Point(0, 0);
+            this.ListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(739, 664);
+            this.ListBox.TabIndex = 0;
+            this.ListBox.DoubleClick += new System.EventHandler(this.ListBox_DoubleClick);
+            // 
+            // OkButton
+            // 
+            this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OkButton.Location = new System.Drawing.Point(13, 679);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(344, 48);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "&OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseButton.Location = new System.Drawing.Point(363, 679);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(364, 48);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "&Cancel";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SelectForm
+            // 
+            this.AcceptButton = this.OkButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CloseButton;
+            this.ClientSize = new System.Drawing.Size(739, 739);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.ListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select:";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
