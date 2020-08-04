16b37d08ff11e48dd24abdd7b95504e637873789
Konstantin S <havendv@gmail.com>
2020-08-04 13:43:18 +1000

    Added ArrayUtilities.

---
+++ Net40/Utilities/ArrayUtilities.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+namespace Net40.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ArrayUtilities
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T[] Empty<T>()
+        {
+#if NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2 || NET40 || NET45 || NET452
+            return new T[0];
+#else
+            return System.Array.Empty<T>();
+#endif
+        }
+    }
+}
