43db3a6c4932abd1beb4d38487195a15d136b8b7
Konstantin S <havendv@gmail.com>
2020-08-04 13:44:53 +1000

    Added docs.

--- Net40/Utilities/ArrayUtilities.cs
+++ Net40/Utilities/ArrayUtilities.cs
@@ -3,12 +3,13 @@
 namespace Net40.Utilities
 {
     /// <summary>
-    /// 
+    /// Full Array support for net40.
+    /// <![CDATA[Version: 1.0.0.0]]> <br/>
     /// </summary>
     public static class ArrayUtilities
     {
         /// <summary>
-        /// 
+        /// System.Array.Empty or new T[0].
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
