6af80cf5ebfbaa705480e7f5c72d9b03a7864fef
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-01 15:39:26 +1000

    Added new tests.

--- NetStandard20.Tests/StringExtensionsTests.cs
+++ NetStandard20.Tests/StringExtensionsTests.cs
@@ -7,7 +7,7 @@ namespace NetStandard20.Tests
     public class StringExtensionsTests
     {
         [TestMethod]
-        public void ReplaceAllTest()
+        public void ReplaceAllTest1()
         {
             const string original = @"using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,7 +18,7 @@ namespace NetStandard20.Tests
     [TestClass]
     public class StringExtensionsTests
     {
-#if LICENCING
+#if LICENSING
         [TestMethod]
         public void ReadGetRequestDataAsyncTest()
         {
@@ -29,7 +29,7 @@ namespace NetStandard20.Tests
         }
 #endif
 
-#if LICENCING
+#if LICENSING
         [TestMethod]
         public void ReadGetRequestDataAsyncTest()
         {
@@ -58,7 +58,105 @@ namespace NetStandard20.Tests
 }
 ";
 
-            Assert.AreEqual(expected, original.ReplaceAll("#if LICENCING", "#endif", string.Empty));
+            Assert.AreEqual(expected, original.ReplaceAll("#if LICENSING", "#endif", string.Empty));
+        }
+
+        [TestMethod]
+        public void ReplaceAllTest2()
+        {
+            const string original = @"
+#if LICENSING
+            DaysLeftLabel = new Label
+            {
+                AutoSize = true,
+                Text = @""(Trial Version)"",
+            };
+            PurchaseLinkLabel = new LinkLabel
+            {
+                AutoSize = true,
+                Text = @""Purchase Developer License"",
+#endif
+#if LICENSING && !NET5_0
+                Font = new Font(DaysLeftLabel.Font.FontFamily, 6),
+#endif
+#if LICENSING
+            };
+            PurchaseLinkLabel.LinkClicked += linkLblPurchase_LinkClicked;
+
+            WelcomeBoardPanel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Bottom,
+                Size = new Size(0, 30),
+                FlowDirection = FlowDirection.LeftToRight,
+                Controls =
+                {
+                    PurchaseLinkLabel,
+                    DaysLeftLabel,
+                },
+            };
+
+            Controls.Add(WelcomeBoardPanel);
+
+            WelcomeBoardPanel.ResumeLayout(false);
+            WelcomeBoardPanel.PerformLayout();
+#endif
+";
+            const string expected = @"
+
+
+
+";
+
+            Assert.AreEqual(expected, original.ReplaceAll("#if LICENSING", "#endif", string.Empty));
+        }
+
+        [TestMethod]
+        public void ReplaceAllTest3()
+        {
+            const string original = @"
+#if LICENSING
+            DaysLeftLabel = new Label
+            {
+                AutoSize = true,
+                Text = @""(Trial Version)"",
+            };
+            PurchaseLinkLabel = new LinkLabel
+            {
+                AutoSize = true,
+                Text = @""Purchase Developer License"",
+#endif
+#if LICENSING && !NET5_0
+                Font = new Font(DaysLeftLabel.Font.FontFamily, 6),
+#endif
+#if LICENSING
+            };
+            PurchaseLinkLabel.LinkClicked += linkLblPurchase_LinkClicked;
+
+            WelcomeBoardPanel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Bottom,
+                Size = new Size(0, 30),
+                FlowDirection = FlowDirection.LeftToRight,
+                Controls =
+                {
+                    PurchaseLinkLabel,
+                    DaysLeftLabel,
+                },
+            };
+
+            Controls.Add(WelcomeBoardPanel);
+
+            WelcomeBoardPanel.ResumeLayout(false);
+            WelcomeBoardPanel.PerformLayout();
+#endif
+";
+            const string expected = @"
+#if LICENSING#endif
+#if LICENSING#endif
+#if LICENSING#endif
+";
+
+            Assert.AreEqual(expected, original.ReplaceAll("#if LICENSING", "#endif", string.Empty, false));
         }
     }
 }
